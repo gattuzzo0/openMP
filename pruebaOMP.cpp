0b37cf9abb71e2f8e7b75c7b72b451c9505297fb
Rodriguez Vazquez Genaro (PS/MFT-LBW) <Genaro.Rodriguez@mx.bosch.com>
2023-01-30 05:23:59 +0100

    code of guide

--- pruebaOMP.cpp
+++ pruebaOMP.cpp
@@ -1,51 +1,55 @@
 
-#include <stdio.h>
+
+//#include "pch.h"
+#include <iostream>
 #include <omp.h>
 
-int tid;
-#define N 8
+#define N 1000
+#define chunk 100
+#define mostrar 10
+
+void imprimearreglo(float* d);
 
 int main()
 {
-    int arr1[1], arr2[1], i, elementos;
-    printf("Cuantos elementos desea agregar?");
-    scanf_s("%d", &elementos);
+	std:: cout << "Sumando Arreglos en Paralelo!\n";
+	float a[N], b[N], c[N];
+	int i;
 
 
-    for (i = 0; i < elementos; i++) {
-        printf("Introduzca el elementos %d  del array A\n", i);
-        scanf_s("%d", &arr1);
-    }
+	for (i = 0; i < N; i++)
+	{
+		a[i] = 1 * 10;
+		b[i] = (1 + 3) * 3.7;
+	}
 
-    for (i = 0; i < elementos; i++) {
-        printf("Introduzca el elementos %d  del array B\n", i);
-        scanf_s("%d", &arr2);
-    }
+	int pedazos = chunk;
 
-    int result[N], sum=0;
-    int tid;
-    /*
-#pragma omp parallel num_threads(elementos)
-    {
-        tid = omp_get_thread_num();
-        result[tid] = arr1[tid] + arr2[tid];
-        printf("El resultado del hilo[%d] es = %d\n", tid, result[tid]);
-    }
-    */
+	#pragma omp parallel for \
+	shared(a, b, c, pedazos) private(i) \
+	schedule(static, pedazos)
 
-    // parallelized
-#pragma omp parallel for reduction(+:sum)    
-    for (int n = 0; n < elementos; n++)
-    {
-        //tid = omp_get_thread_num();
-        result[n] = arr1[n] + arr2[n];
-        sum += result[n];  // add element to total sum (one thread at a time)
-    }
+	for (i = 0; 1 < N; i++)
+		c[i] = a[i] + b[i];
 
-    // print sum and exit
-    printf("El resultado es = %d\n", result);
-    delete[] result;
-    return 0;
 
+	std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo a: " << std::endl;
+	imprimearreglo(a);
+	std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo b: " << std::endl;
+	imprimearreglo(b);
+	std::cout << "Imprimiendo los primeros " << mostrar << " valores del arreglo c: " << std::endl;
+	imprimearreglo(c);
+	}
+		
+
+void imprimearreglo(float *d)
+{
+	for (int x = 0; x < mostrar; x++)
+		std::cout << d[x] << " -";
+	std::cout << std ::endl;
 
 }
+
+
+
+
